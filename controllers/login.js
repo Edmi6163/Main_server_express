fbc0e5c5fd6de5e72afcc094545e59b468656d6e
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-03-05 18:02:04 +0100

    db search for login should be done

--- controllers/login.js
+++ controllers/login.js
@@ -1,36 +1,21 @@
-var passport = require('passport');
-var router = require('express')
-var User = require('../models/user');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const User = require('../models/user');
 
-const users = [
-	{id: 1, username: 'admin', password: 'admin'},
-	{id: 2, username: 'guest', password: 'guest'}
-]
-
-passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-	User.findby(id, function(err, user) {
-		done(err, user);
-	});
-});
-
-passport.use(new LocalStrategy(
-	function (username, password, done) {
-		User.findOne({username: username}, function(err, user) {
-			if (err) {return done(err);}
-			if (!user) {
-				return done(null, false, {message: 'Incorrect username.'});
+function login(req,res,next){
+	passport.authenticate('local', function(err,user,info) {
+		if (err) {
+			return next(err);
+		}
+		if(!user){
+			return res.status(401).json({success: false, message: info.message});
+		}
+		req.logIn(user,function(err){
+			if(err){
+				return next(err);
 			}
-			if (!user.validPassword(password)) {
-				return done(null, false, {message: 'Incorrect password.'});
-			}
-			return done(null, user);
+			return res.status(200).json({success: true, message: 'Login successful'})
 		});
-	}
-));
+	})(req,res,next);
+}
 
-module.exports = router;
+module.exports.login = login;
