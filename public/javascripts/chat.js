1de78c20dd90ad8f0d45a1b574e8ae8ce90cd6df
Riccardo Oro <riccardo.oro@edu.unito.it>
2024-03-05 12:22:13 +0100

    Commit some chat system function (to test)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -21,4 +21,39 @@ function init() {
         currentroom = null;
     });
 
+    let field = document.getElementById('message_input');
+    field.addEventListener('keypress',(event)=>{
+        if(event.key ==='Enter')
+        {
+            socket.emit('chat message ',currentroom,message_input.value , Username); //send the message
+            message_input.value = ''; //reset the message string
+            return false;
+        }
+    });
+    //who send the message + message text
+    socket.on('chat message ',(msg,username)=>{
+       let who = (name === Username) ? "Me" :name;
+       const li =document.getElementById('li');
+       li.textContent = who +"-"+msg;
+       messages.appendChild(li);
+    });
+    //user enter  the conversation
+    socket.on('create or join conversation',(name)=>{
+        if(name ===Username) return ;
+        const li =document.getElementById('li');
+        li.textContent = "User :" +name + "has joined the room";
+        messages.appendChild(li);
+    });
+    //get this information from browser memeory
+    Username = localStorage.getItem('my_name');
+    currentroom = localStorage.getItem('room');
+    if(Username)
+    {
+         //Take the username from Login or Signup Form
+         document.getElementById('SemailUsername').value =Username;
+         document.getElementById('emailUsername').value =Username;
+    }
+    document.getElementById("form_container").style.display = 'block';
+    document.getElementById("message_container").style.display='none';
+    document.getElementById('logout').style.display='none';
 }
