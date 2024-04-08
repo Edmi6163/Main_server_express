b4fb69c3a052b9b4859e227ffbc807bbfb3e518a
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-04-08 17:11:01 +0200

    added csvtojson lib to load csv file

--- ExpressConnect/controller/importController.js
+++ ExpressConnect/controller/importController.js
@@ -1,41 +1,23 @@
-const fs = require('fs');
+const csvtojson = require('csvtojson');
 const Games = require('../models/games');
 
 const file_path = '../Assignment_Data_2023-2024/game_events.csv'
 
-const importData = (req, res) => {
-	fs.createReadStream(file_path).pipe(csv()).on('data', (row) => {
-			const games = new Games({
-				game_id: row.game_id,
-				competition_id: row.competition_id,
-				season: row.season,
-				round: row.round,
-				date: row.date,
-				home_club_id: row.home_club_id,
-				away_club_id: row.away_club_id,
-				home_club_goals: row.home_club_goals,
-				away_club_goals: row.away_club_goals,
-				home_club_position: row.home_club_position,
-				away_club_position: row.away_club_position,
-				home_club_manager_name: row.home_club_manager_name,
-				away_club_manager_name: row.away_club_manager_name,
-				stadium: row.stadium,
-				attendance: row.attendance,
-				referee: row.referee,
-				url: row.url,
-				home_club_formation: row.home_club_formation,
-				away_club_formation: row.away_club_formation,
-				home_club_name: row.home_club_name,
-				away_club_name: row.away_club_name,
-				aggregate: row.aggregate,
-				competition_type: row.competition_type,
-			});
-			game.save().then(() => console.log('game saved')).catch((err) => console.error('Error saving game', err));
+const importData = async (req, res) => {
+	try {
+		const games = await  csvtojson().fromFile(file_path);
+		await  Games.insertMany(games);
+		res.status(200).json({
+			status: 'success',
+			data: games,
+		});
+	} catch (err) {
+		res.status(500).json({
+			status: 'fail',
+			message: err.message,
+		});
+	}
 
-			}).on('end', () => {
-				console.log('CSV file successfully processed');
-				res.send('Data imported');
-			});
 };
 
 module.exports = {importData};
