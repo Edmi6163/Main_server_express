229676f12540938d6d4b7e79401fe1176be18b81
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-04-14 13:49:47 +0200

    created controller and route for games.csv

--- ExpressConnect/routes/index.js
+++ ExpressConnect/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const importGameEventsController = require('../controller/gameEventsController');
 const importGameLineUpController = require('../controller/gameLineUpsController');
+const importGamesController = require('../controller/gamesController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,3 +15,4 @@ router.post('/importGameEvents',importGameEventsController.importData);
 
 router.post('/importGameLineUp',importGameLineUpController.importData);
 
+router.post('/importGames',importGamesController.importData);
