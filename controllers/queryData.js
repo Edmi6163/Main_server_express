f090e0d3ed32386a50cf79e3789934bf6b81db25
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-05-06 17:21:00 +0200

    fixed axios error

--- controllers/queryData.js
+++ controllers/queryData.js
@@ -1,8 +1,8 @@
-
+const axios = require('axios');
 async function queryInfo(req,res){
 		try {
 				const data = req.body;
-				const response = await axios.post('http://localhost:3001/query/',data);
+				const response = await axios.post('http://localhost:3001/query',data);
 				res.json(response.data);
 		} catch (error) {
 				res.status(500).json({error: error.message});
