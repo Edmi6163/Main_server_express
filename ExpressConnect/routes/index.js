955185db51436ded3bb9bf40b2031383bbdbb491
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-05-09 10:33:03 +0200

    rollback

--- ExpressConnect/routes/index.js
+++ ExpressConnect/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const importGameEventsController = require('../controller/gameEventsController');
 const importGameLineUpController = require('../controller/gameLineUpsController');
 const importGamesController = require('../controller/gamesController');
-const dispatcher = require('../controller/adapter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,12 +16,3 @@ router.post('/importGameEvents',importGameEventsController.importData);
 router.post('/importGameLineUp',importGameLineUpController.importData);
 
 router.post('/importGames',importGamesController.importData);
-
-router.get('/query', async (req, res) => {
-  try {
-    const queryRes = await dispatcher(req,res);
-    res.json(queryRes);
-  } catch(error) {
-    res.status(500).json({error: error.message});
-  }
-});
