7316d0dd7e67b793d9a5084ba5c6814c7350a50a
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-05-11 19:44:55 +0200

    fixed request in second server error

--- models/NoSqlModels.js
+++ models/NoSqlModels.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 const {response} = require("express");
 
-const EXPRESS_URL = 'https://localhost:3001';
+const EXPRESS_URL = 'http://localhost:3001';
 
 async function executeNoSql(query) {
 	try {
-		const res = await axios.get(EXPRESS_URL, {params: {query}});
+		const res = await axios.post(EXPRESS_URL, {params: {query}});
 		return res.data;
 	} catch (error) {
 		throw error;
--- models/SqlModels.js
+++ models/SqlModels.js
@@ -1,9 +1,12 @@
 const axios = require('axios')
+const https = require('https')
 
-const SPRING_BOOT_URL = 'https://localhost:8081'
+const SPRING_BOOT_URL = 'http://localhost:8081'
+
+
 async function executeSqlQuery(query) {
 	try {
-		const res = await axios.get(SPRING_BOOT_URL, {params: {query}});
+		const res = await axios.post(SPRING_BOOT_URL, {params: {query}});
 		return res.data;
 	} catch (error) {
 		throw error;
