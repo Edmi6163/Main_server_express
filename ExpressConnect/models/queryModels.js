92c5a9d9ecc59cb45bfa4dc655701cc7b9c54dc3
eleonora valeri <eleonora.valeri@edu.unito.it>
2024-06-03 17:55:13 +0200

    projection

--- ExpressConnect/models/queryModels.js
+++ ExpressConnect/models/queryModels.js
@@ -1,20 +1,24 @@
 const GameLineUps = require('../models/gameLineups')
 const Games = require('../models/games')
 const GamesEvents = require('../models/gamesEvents')
+const Clubs = require('../models/clubs')
 
 async function queryIdentifier(req) {
 	try {
-		const { collection, query} = req.body.query;
+		const { collection, query, projection} = req.body.query;
 		let res;
 		switch (collection){
 			case 'games':
-				res = await Games.find(query);
+				res = await Games.find(query, projection);
 				break;
 			case 'gameLineUps':
-				res = await GameLineUps.find(query);
+				res = await GameLineUps.find(query, projection);
 				break;
 			case 'gameEvents':
-				res = await GamesEvents.find(query);
+				res = await GamesEvents.find(query, projection);
+				break;
+			case 'clubs':
+				res = await Clubs.find(query, projection);
 				break;
 			default:
 				throw  new Error('Invalid query type');
