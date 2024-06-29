4a2ebde8c4550d28cff1b35d198612f218d18b66
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-06-29 11:35:14 +0200

    optimized performance

--- ExpressConnect/controller/scoreboardsController.js
+++ ExpressConnect/controller/scoreboardsController.js
@@ -1,54 +1,76 @@
 const Games = require('../models/games')
+const quickSort = (arr, compareFn) => {
+	if (arr.length <= 1) return arr;
+	const [pivot, ...rest] = arr;
+	const left = rest.filter(el => compareFn(el, pivot) < 0);
+	const right = rest.filter(el => compareFn(el, pivot) >= 0);
+	return [...quickSort(left, compareFn), pivot, ...quickSort(right, compareFn)];
+};
 
-function quickSort(arr,compare){
-	if(arr.length <= 1){
-		return arr;
-	}
+const calculateScore = async () => {
+	try {
+		const games = await Games.find();
+		const board = {};
 
-	const pivot = arr[arr.length -1];
-	const left = [];
-	const right = [];
+		games.forEach(game => {
+			const { home_club_name, away_club_name, home_club_goals, away_club_goals } = game;
+			if (!board[home_club_name]) {
+				board[home_club_name] = {
+					team: home_club_name,
+					points: 0,
+					goals: 0,
+					goalsAgainst: 0,
+					matches: 0,
+					won: 0,
+					drawn: 0,
+					lost: 0
+				};
+			}
+			if (!board[away_club_name]) {
+				board[away_club_name] = {
+					team: away_club_name,
+					points: 0,
+					goals: 0,
+					goalsAgainst: 0,
+					matches: 0,
+					won: 0,
+					drawn: 0,
+					lost: 0
+				};
+			}
 
-	for(let i = 0; i < arr.length -1; i++){
-		if(compare(arr[i],pivot) < 0){
-			left.push(arr[i]);
-		} else {
-			right.push(arr[i]);
-		}
-	}
-	return [...quickSort(left,compare),pivot,...quickSort(right,compare)];
-}
+			board[home_club_name].matches++;
+			board[away_club_name].matches++;
 
-const calculateScore = async() => {
-	const games = await Games.find();
-	const board = {};
+			board[home_club_name].goals += home_club_goals;
+			board[home_club_name].goalsAgainst += away_club_goals;
+			board[away_club_name].goals += away_club_goals;
+			board[away_club_name].goalsAgainst += home_club_goals;
 
-	games.forEach(game => {
-		const {home_club_name,away_club_name,home_club_goals,away_club_goals} = game;
-		if(!board[home_club_name]) board[home_club_name] = {team: home_club_name, points: 0, goals: 0, goalsAgainst: 0, matches: 0};
-		if(!board[away_club_name]) board[away_club_name] = {team: away_club_name, points: 0, goals: 0, goalsAgainst: 0, matches: 0};
+			if (home_club_goals > away_club_goals) {
+				board[home_club_name].points += 3;
+				board[home_club_name].won++;
+				board[away_club_name].lost++;
+			} else if (home_club_goals < away_club_goals) {
+				board[away_club_name].points += 3;
+				board[away_club_name].won++;
+				board[home_club_name].lost++;
+			} else {
+				board[home_club_name].points++;
+				board[away_club_name].points++;
+				board[home_club_name].drawn++;
+				board[away_club_name].drawn++;
+			}
+		});
 
-		board[home_club_name].played++;
-		board[away_club_name].played++;
-
-		if(home_club_goals > away_club_goals){
-			board[home_club_name].points += 3;
-			board[home_club_name].won++;
-			board[away_club_name].lost++;
-		} else if(home_club_goals < away_club_goals){
-			board[away_club_name].points += 3;
-			board[away_club_name].won++;
-			board[home_club_name].lost++;
-		} else {
-			board[home_club_name].points++;
-			board[away_club_name].points++;
-			board[home_club_name].drawn++;
-			board[away_club_name].drawn++;
-		}
-		const boardArray = Object.values(board)
+		const boardArray = Object.values(board);
 		return quickSort(boardArray, (a, b) => b.points - a.points);
+	} catch (err) {
+		console.error('Error calculating score:', err);
+		throw err;
+	}
+};
 
-	})
-}
+
 
 module.exports = {calculateScore}
